d27ff9d7c62007e614675b1b1e503a464b082ce4
hadoobidoop <hadoobidoop@gmail.com>
2020-09-26 23:35:12 +0900

    [ADD]새로운 할인 정책 개발

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discoutPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discoutPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
