a8e898f691e134ed7d059fe7d3e3c9a06ba2793f
hadoobidoop <hadoobidoop@gmail.com>
2020-10-10 23:04:48 +0900

    [ADD]탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,7 +7,12 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 //@Component 어노테이션이 붙은 객체를 찾아서 빈으로 등록해준다.
 //기존 예제 코드인 AppConfig를 살리기위해서 excludeFilter로 컴포넌트 스캔을 안하도록 설정했다.
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+@ComponentScan(
+        //basePackages를 통한 탐색할 위치 지정
+        //*권장! 패키지 위치를 지정하지 않고, 설정 정보 클래스의 위치를 프로젝트 최상단에 두기!!
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 public class AutoAppConfig {
 
 }
